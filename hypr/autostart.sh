3203dbea15d0c47677de4cb019cfc1ddbb09c542
Max <emirshn000@gmail.com>
2025-08-25 15:19:51 +0300

    Delete hypr directory

--- hypr/autostart.sh
+++
@@ -1,8 +0,0 @@
-if [ -f "$HOME/.config/.current_wallpaper" ]; then
-    swww img "$(cat $HOME/.config/.current_wallpaper)" \
-        --transition-type grow \
-        --transition-pos top-left \
-        --transition-fps 60 \
-        --transition-bezier 0.25,0.1,0.25,1
-    wallust -i "$(cat $HOME/.config/.current_wallpaper)"
-fi
